115b085d8e6e5525eb75f77846b9f17cac0db137
htsi <htsi@com>
2018-08-12 22:07:53 +0800

    inner product, prompt user to continue

--- src/ip.cpp
+++ src/ip.cpp
@@ -8,14 +8,22 @@ void getVectorFromConsole (double v[], int n) {
 }
 
 int main(int argc, char ** argv) {
-  std::cout << "input dimension of the vectors for inner product computation:\n";
-  int n;
-  std::cin >> n;
-  double u[n], v[n];
-  std::cout << "input the first vector:\n";
-  getVectorFromConsole(u, n);
-  std::cout << "input the second vector:\n";
-  getVectorFromConsole(v, n);
-  std::cout << "the inner product is " << innerProduct(u, v, n) << "\n";
+  bool cont = true;
+  while (cont) {
+    std::cout << "input dimension of the vectors for inner product computation:\n";
+    int n;
+    std::cin >> n;
+    double u[n], v[n];
+    std::cout << "input the first vector:\n";
+    getVectorFromConsole(u, n);
+    std::cout << "input the second vector:\n";
+    getVectorFromConsole(v, n);
+    std::cout << "the inner product is " << innerProduct(u, v, n) << "\n";
+    std::cout << "Press c<ret> to continue, anything else<ret> to terminate: ";
+    char reply;
+    std::cin >> reply;
+    cont = ('c' == reply);
+  }
+  std::cout << "Goodbye from inner product calculator!\n";
   return 0;
 }
